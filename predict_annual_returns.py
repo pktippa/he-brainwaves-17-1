606046b935b682500d5d666ce35633e88f3f5ff1
Pradeep Kumar Tippa <pktippa@gmail.com>
2017-12-20 20:53:52 +0530

    Updated predict annual returns file

--- predict_annual_returns.py
+++ predict_annual_returns.py
@@ -1,6 +1,9 @@
+# Predicting Annual returns
+from pathlib import Path
 # Importing numpy
 import numpy as np
-from pathlib import Path
+import pandas as pd
+from sklearn.cross_validation import KFold
 
 # Setting up train data file path and resolving absoulte path.
 train_data_file_path = 'data/predict_annual_returns/train.csv'
@@ -18,13 +21,30 @@ test_data_file_path = str(test_data_file_path)
 train_data = np.genfromtxt(train_data_file_path, dtype=None, delimiter=',', names=True)
 test_data = np.genfromtxt(test_data_file_path, dtype=None, delimiter=',', names=True)
 
+print(train_data)
+print(train_data.shape)
+#print('First Row', train_data[:,0])
+
 # Required label name
-label_name = 'return_'
+label_name = 'return'
 
 # Extracting label from data 
-y_train = train_data[label_name]
+y = train_data[label_name]
 
 # Removing label from train_data using list compression
-X_train = train_data[[b for b in list(train_data.dtype.names) if b != label_name]]
+X = train_data[[b for b in list(train_data.dtype.names) if b != label_name]]
+
+print('original shape X ', X.shape, ' y ', y.shape)
 
 # Now splitting the training set into train and validation sets.
+
+eval_size = 0.10
+kf = KFold(len(y), round(1. / eval_size))
+train_indices, valid_indices = next(iter(kf))
+X_train, y_train = X[train_indices], y[train_indices]
+X_valid, y_valid = X[valid_indices], y[valid_indices]
+
+print('Shape after splitting train and validation data ', 
+    'X_train ' , X_train.shape, ' y_train ', y_train.shape,
+    'X_valid ' ,X_valid.shape, ' y_valid ', y_valid.shape,)
+
